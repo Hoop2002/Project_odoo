7b899588e30b0eaae9946cb9bb3e8455b23ee7aa
Hoop2002 <hoopertygoas@gmail.com>
2023-09-20 13:45:16 +0000

    UPD: odoo sales

--- addons/lineupmmc_sales/models/res_company.py
+++ addons/lineupmmc_sales/models/res_company.py
@@ -2,21 +2,6 @@
 
 from odoo import models, fields, api
 
-
-# class lineupmmc_sales(models.Model):
-#     _name = 'lineupmmc_sales.lineupmmc_sales'
-#     _description = 'lineupmmc_sales.lineupmmc_sales'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
-
 class Company(models.Model):
     _inherit = 'res.company'
 
